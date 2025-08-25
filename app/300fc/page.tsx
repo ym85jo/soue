dc844e6790cd552972c67d4c0a2a9e352d26bfe1
Jo <ehil@nate.com>
2025-08-25 23:12:45 +0900

    ''

--- app/300fc/page.tsx
+++ app/300fc/page.tsx
@@ -29,7 +29,6 @@ function createDefaultTeamColors(teamCount: number) {
 
 function getToday() {
   const today = new Date();
-  const yy = String(today.getFullYear()).slice(-2);
   const mm = String(today.getMonth() + 1).padStart(2, "0");
   const dd = String(today.getDate()).padStart(2, "0");
   return `${mm}.${dd}`;
